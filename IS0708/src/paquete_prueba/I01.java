0306f868b9e36895bc22174bd5868f9171648061
mrzeon <mrzeon@41ee696f-9e3d-0410-b01d-1107f526e5c3>
2007-11-02 00:19:45 +0000

    Creación I-01.
    -Daniel

---
+++ IS0708/src/paquete_prueba/I01.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * INTERFAZ I-01 :: Identificación
+ *   por Daniel Dionne
+ *   
+ * Interfaz de identificación del usuario.
+ *******************************************************************************/
+
+
+package paquete_prueba;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.*;
+
+public class I01 {
+
+	public static void main(String[] IS0708) {
+		final Display display = new Display ();
+		final Shell shell = new Shell (display);
+
+		final Label lUsuario = new Label(shell, SWT.LEFT);
+		final Text tUsuario  = new Text(shell, SWT.BORDER);
+		final Label lPassword = new Label(shell, SWT.LEFT);
+		final Text tPassword = new Text(shell, SWT.BORDER);
+
+	
+		final Button bAceptar = new Button(shell, SWT.PUSH);
+		final Button bCancelar = new Button(shell, SWT.PUSH);
+
+		shell.setText("Identificación del usuario");
+		shell.setVisible(true);
+
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 8;
+		layout.makeColumnsEqualWidth = true;
+
+		shell.setLayout(layout);
+
+		lUsuario.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true, 3, 1));
+		lUsuario.setText("Vendedor");
+		tUsuario.setEditable(true);
+		tUsuario.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true, 5, 1));
+
+		lPassword.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true, 3, 1));
+		lPassword.setText("Contraseña");
+		tPassword.setEditable(true);
+		tPassword.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true, 5, 1));
+
+		bAceptar.setText("Aceptar");
+		bAceptar.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true, 4, 1));
+		
+		bCancelar.setText("Cancelar");
+		bCancelar.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true, 4, 1));
+
+		// Un SelectionAdapter con lo que hace el botón
+		SelectionAdapter sabCancelar = new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				shell.dispose();
+			}
+		};
+
+		bCancelar.addSelectionListener(sabCancelar);
+
+		shell.pack();
+		shell.setSize(200, 150);
+		shell.setLocation(500, 400);
+		shell.open();
+
+		// Este bucle mantiene la ventana abierta
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		display.dispose();
+	}
+}
