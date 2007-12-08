1bb7bbb35ca0e43a6821218db18fd16d56d9cb43
elsmda <elsmda@41ee696f-9e3d-0410-b01d-1107f526e5c3>
2007-12-08 17:10:15 +0000

    Esqueleto Clase Incidencia (atributos, getters&setters)

---
+++ IS0708/src/algoritmo/Incidencia.java
@@ -0,0 +1,73 @@
+package algoritmo;
+import java.util.Date;
+
+/**
+ * Representación de posibles incidencias de los trabajadores
+ * @author Agustín Daniel Delgado Muñoz
+ */
+public class Incidencia {
+	
+	private int nvend;
+	private int idIncidencia;
+	private String descripcion;
+	private Date fechainicio= new Date();
+	private Date fechafin=new Date();
+	
+	
+	public Incidencia(){ //constructora por defecto
+		
+	}
+	
+	public Incidencia(int user, int id, String descr,Date finicio, Date ffin){
+		this.nvend=user;
+		this.idIncidencia=id;
+		this.descripcion=descr;
+		this.fechainicio=finicio;
+		this.fechafin=ffin;
+	}
+	
+	//getters & setters de la clase Incidencias
+	
+	public int getnvend() {
+		return this.nvend;
+	}
+	public void setNvend(int user) {
+		this.nvend = user;
+	}
+	
+	public int getIDincidencia() {
+		return this.idIncidencia;
+	}
+	public void setIDincidencia(int id) {
+		this.idIncidencia = id;
+	}
+	
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+	public void setDescripcion(String descr) {
+		this.descripcion=descr;
+	}
+	
+	
+	public Date getFechaInicio() {
+		return this.fechainicio;
+	}
+	public void setFechaInicio(Date finicio) {
+		this.fechainicio=finicio;
+	}
+	
+	
+	public Date getFechaFin() {
+		return this.fechafin;
+	}
+	public void setFechaFin(Date ffin) {
+		this.fechafin=ffin;
+	}
+	
+	
+	
+	
+	
+
+}
