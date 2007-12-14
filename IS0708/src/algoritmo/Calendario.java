0035bb13639a184a34c135cc638826680fb6b063
madctol <madctol@41ee696f-9e3d-0410-b01d-1107f526e5c3>
2007-12-14 16:36:59 +0000

    Añadidos comentarios javadoc a todos los metodos Creación de más metodos getters y setters utilizar comodamente cada hora.

--- IS0708/src/algoritmo/Calendario.java
+++ IS0708/src/algoritmo/Calendario.java
@@ -12,64 +12,108 @@ public class Calendario {
 	
 	private int mes;
 	private int anio;
-	private int numDias;//numero de dias del mes
-	private HoraCalendario cal[][];//esta matriz contiene para cada dia y para cada hora un objeto de la clase Horacalendario
+	private int numDias; //numero de dias del mes
+	private HoraCalendario cal[][]; //esta matriz contiene para cada dia y para cada hora un objeto de la clase Horacalendario
 	
-	public Calendario(){//constructora por defecto
+	/**
+	 * Constructora por defecto
+	 */
+	public Calendario(){
 		mes = 0;
 		anio = 0;
 		numDias = 0;
 		cal = null;
 	}
 	
+	/**
+	 * Constructora de un objeto Calendario
+	 * @param mes
+	 * @param anio
+	 */
 	public Calendario(int mes,int anio){
 		this.mes = mes;
 		this.anio = anio;
-		numDias = Util.dameDias(mes,anio);
-		cal=new HoraCalendario[numDias][24];//el calendario tendra un numero de dias dependiendo del mes y de 0 a 23 horas
+		numDias = Util.dameDias(mes,anio); 
+		//el calendario tiene un numero de dias segun el mes y de 0 a 23 horas
+		cal = new HoraCalendario[numDias][24]; 
 		//ahora inicializamos el calendario
 		for (int i=0;i<numDias;i++){
 			for (int j=0;j<24;j++){
-				//Aqui debemos rellenar los datos accediendo a las base de datos
+				cal[i][j]=new HoraCalendario(0,0,0,0);
 				//PRUEBA
 				//cal[i][j]=new HoraCalendario(4,0,0,0);
-				cal[i][j]=new HoraCalendario(0,0,0,0);
 			}
 		}
 		
 	}
 	
+	/**
+	 * Consulta el mes del calendario
+	 * @return mes
+	 */
 	public int getMes() {
 		return mes;
 	}
 	
+	/**
+	 * Modificadora del mes del calendario
+	 * @param mes Nuevo valor para mes
+	 */
 	public void setMes(int mes) {
 		this.mes = mes;
 	}
 	
+	/**
+	 * Consulta el año del calendario
+	 * @return año
+	 */
 	public int getAnio() {
 		return anio;
 	}
 	
+	/**
+	 * Consulta el año del calendario
+	 * @return Año
+	 */
 	public void setAnio(int anio) {
 		this.anio = anio;
 	}
 	
+	/**
+	 * Consulta el numero de dias del mes del calendario
+	 * @return Numero de dias
+	 */
 	public int getNumDias() {
 		return numDias;
 	}
 	
+	/**
+	 * Modifica el numero de dias del calendario
+	 * @param numDias Nuevo numero de dias
+	 */
 	public void setNumDias(int numDias) {
 		this.numDias = numDias;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 * @deprecated 
+	 */
 	public HoraCalendario[][] getCal() {
 		return cal;
 	}
 	
+	
+	/**
+	 * 
+	 * @param cal
+	 * @deprecated
+	 */
 	public void setCal(HoraCalendario[][] cal) {
 		this.cal = cal;
 	}
+	
 	
 	/**
 	 * Actualiza la información de una hora concreta del calendario
@@ -89,31 +133,53 @@ public class Calendario {
 		}
 	}
 	
+	/**
+	 * Consulta el numero maximo de empleados para un dia y una hora del calendario
+	 * @return Numero maximo de empleados
+	 */
 	public int getMaxHora(int dia, int hora){
 		if (esCorrecto(dia, hora))
 			return cal[dia][hora].getMax();
 		else return -1;
 	}
 	
+	/**
+	 * Consulta el numero minimo de empleados para un dia y una hora del calendario
+	 * @return Numero minimo de empleados
+	 */
 	public int getMinHora(int dia, int hora){
 		if (esCorrecto(dia, hora))
 			return cal[dia][hora].getMin();
 		else return -1;
 		}
 	
+	/**
+	 * Consulta el numero de expertos del patron para un dia y una hora del calendario
+	 * @return numero de expertos
+	 */
 	public int getExpHora(int dia, int hora){
 		if (esCorrecto(dia, hora))
 			return cal[dia][hora].getExpertos();
 		else return -1;
 		}
 	
+	/**
+	 * Consulta el numero de principiantes del patron para un dia y una hora del calendario
+	 * @return numero de principiantes
+	 */
 	public int getPrincHora(int dia, int hora){
 		if (esCorrecto(dia, hora))
 			return cal[dia][hora].getPrincipiantes();
 		else return -1;
 		}
 	
-	public boolean esCorrecto(int dia, int hora){
+	/**
+	 * Metodo para comprobar si los indices con los que se accede al calendario son correctos
+	 * @param dia
+	 * @param hora
+	 * @return true si 1<=dia<=numDias y 0<=hora<24
+	 */
+	private boolean esCorrecto(int dia, int hora){
 		return (dia>=1 && dia<=numDias && hora>=0 && hora<24);
 	}
 }
