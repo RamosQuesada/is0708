085aacb6754e8372be1c7fe7365bbe8d70cf141b
madctol <madctol@41ee696f-9e3d-0410-b01d-1107f526e5c3>
2008-04-29 10:42:01 +0000

    Cambio de ubicacion de las imagenes

--- IS0708/src/interfaces/imagenes/CargadorImagenes.java
+++ IS0708/src/interfaces/imagenes/CargadorImagenes.java
@@ -1,41 +1,50 @@
 package interfaces.imagenes;
 
+import java.io.File;
+
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
+
+import aplicacion.utilidades.Util;
 
 public class CargadorImagenes {
 	private Image icoGr, icoPq, icoBk, icoFw, ico_home, ico_imprimir, ico_cuadrante, ico_chico,
 	ico_chica, ico_chicos, fondoLogin, ico_mens_l, ico_mens, fondoAdmin, grafica,
-	carga1,carga2,carga3,carga4,carga5,carga6,carga7;		
+	carga1,carga2,carga3,carga4,carga5,carga6,carga7;
+	
+	private String rutaImagenes = Util.rutaTrabajo() + File.separator + "Imagenes";
 	
 	public CargadorImagenes(Display display) {
-		icoGr				= new Image(display, CargadorImagenes.class.getResourceAsStream("icoGr.gif"));
-		icoPq				= new Image(display, CargadorImagenes.class.getResourceAsStream("icoPq.gif"));
-		icoBk               = new Image(display, CargadorImagenes.class.getResourceAsStream("ico_bk.gif"));
-		icoFw               = new Image(display, CargadorImagenes.class.getResourceAsStream("ico_fw.gif"));
-		ico_home            = new Image(display, CargadorImagenes.class.getResourceAsStream("ico_home.gif"));
-		ico_imprimir		= new Image(display, CargadorImagenes.class.getResourceAsStream("ico_imprimir.gif"));
-		ico_cuadrante		= new Image(display, CargadorImagenes.class.getResourceAsStream("ico_cuadrante.gif"));
-		ico_chico			= new Image(display, CargadorImagenes.class.getResourceAsStream("ico_chico.gif"));
-		ico_chica			= new Image(display, CargadorImagenes.class.getResourceAsStream("ico_chica.gif"));
-		ico_chicos			= new Image(display, CargadorImagenes.class.getResourceAsStream("ico_chicos.gif"));
-		fondoLogin			= new Image(display, CargadorImagenes.class.getResourceAsStream("intro.png"));
-		ico_mens_l			= new Image(display, CargadorImagenes.class.getResourceAsStream("ico_mens1_v.gif"));
-		ico_mens			= new Image(display, CargadorImagenes.class.getResourceAsStream("ico_mens2_v.gif"));
-		fondoAdmin			= new Image(display, CargadorImagenes.class.getResourceAsStream("admin.png"));
-		grafica				= new Image(display, CargadorImagenes.class.getResourceAsStream("grafica_1.ico"));
-		carga1				= new Image(display, CargadorImagenes.class.getResourceAsStream("espere01.png"));
-		carga2				= new Image(display, CargadorImagenes.class.getResourceAsStream("espere02.png"));
-		carga3				= new Image(display, CargadorImagenes.class.getResourceAsStream("espere03.png"));
-		carga4				= new Image(display, CargadorImagenes.class.getResourceAsStream("espere04.png"));
-		carga5				= new Image(display, CargadorImagenes.class.getResourceAsStream("espere05.png"));
-		carga6				= new Image(display, CargadorImagenes.class.getResourceAsStream("espere06.png"));
-		carga7				= new Image(display, CargadorImagenes.class.getResourceAsStream("espere07.png"));
+		icoGr				= new Image(display, rutaImagenes + File.separator + "icoGr.gif");
+		icoPq				= new Image(display, rutaImagenes + File.separator + "icoPq.gif");
+		icoBk               = new Image(display, rutaImagenes + File.separator + "ico_bk.gif");
+		icoFw               = new Image(display, rutaImagenes + File.separator + "ico_fw.gif");
+		ico_home            = new Image(display, rutaImagenes + File.separator + "ico_home.gif");
+		ico_imprimir		= new Image(display, rutaImagenes + File.separator + "ico_imprimir.gif");
+		ico_cuadrante		= new Image(display, rutaImagenes + File.separator + "ico_cuadrante.gif");
+		ico_chico			= new Image(display, rutaImagenes + File.separator + "ico_chico.gif");
+		ico_chica			= new Image(display, rutaImagenes + File.separator + "ico_chica.gif");
+		ico_chicos			= new Image(display, rutaImagenes + File.separator + "ico_chicos.gif");
+		fondoLogin			= new Image(display, rutaImagenes + File.separator + "intro.png");
+		ico_mens_l			= new Image(display, rutaImagenes + File.separator + "ico_mens1_v.gif");
+		ico_mens			= new Image(display, rutaImagenes + File.separator + "ico_mens2_v.gif");
+		fondoAdmin			= new Image(display, rutaImagenes + File.separator + "admin.png");
+		grafica				= new Image(display, rutaImagenes + File.separator + "grafica_1.ico");
+		carga1				= new Image(display, rutaImagenes + File.separator + "espere01.png");
+		carga2				= new Image(display, rutaImagenes + File.separator + "espere02.png");
+		carga3				= new Image(display, rutaImagenes + File.separator + "espere03.png");
+		carga4				= new Image(display, rutaImagenes + File.separator + "espere04.png");
+		carga5				= new Image(display, rutaImagenes + File.separator + "espere05.png");
+		carga6				= new Image(display, rutaImagenes + File.separator + "espere06.png");
+		carga7				= new Image(display, rutaImagenes + File.separator + "espere07.png");
 	}
 	
 	public void dispose() {
 		icoGr.dispose();
 		icoPq.dispose();
+		icoBk.dispose();
+		icoFw.dispose();
+		ico_home.dispose();
 		ico_imprimir.dispose();
 		ico_cuadrante.dispose();
 		ico_chico.dispose();
